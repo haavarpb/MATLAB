e1cdcde04a0ee6d761c43bffaa9fff90e8882d81
Håvard <haavard.brandal@gmail.com>
2018-05-24 19:32:00 +0200

    Uploading newest version. Using the pipes from hpb's cellphone we get a good crack detection. Some problems with other images because of the cropping of the frame in addHoughLines()

--- AdvancedTopicsOnComputerVision/ShortProject/addHoughLines.m
+++ AdvancedTopicsOnComputerVision/ShortProject/addHoughLines.m
@@ -1,6 +1,13 @@
 function [I_burned] = addHoughLines(I, hough_lines)
 %ADDHOUGHLINES Burns the plots of hough_lines in hough_lines into the image
 %I
+if size(I, 1) < size(I, 2)
+    s1 = size(I, 1);
+    s2 = size(I, 2);
+else
+    s1 = size(I, 2);
+    s2 = size(I, 1);
+end
 fig = figure('visible', 'off');
 imshow(I);
 hold on;
@@ -8,56 +15,58 @@ for k = 1:length(hough_lines)
     rho = hough_lines(k).rho;
     theta = hough_lines(k).theta;
     xup = rho/cosd(theta);
-    xdown = (rho - size(I, 2)*sind(theta))/cosd(theta);
+    xdown = (rho - s2*sind(theta))/cosd(theta);
     yleft = rho/sind(theta);
-    yright = (rho - size(I, 1)*cosd(theta))/sind(theta);
+    yright = (rho - s1*cosd(theta))/sind(theta) + 1;
     
     % Check if the cut points are valid (different from Inf and -Inf)
     % and plot the line between them
+    
     xy = zeros(2,2);
     j = 1;
-    
-        if (xup ~= Inf) && (xup ~= -Inf)
-            xy(j,:) = [xup, 0];
-            j = j+1;
-            if j==3
-                plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','white');
-                    continue
-                end
+    if (xup ~= Inf) && (xup ~= -Inf)
+        xy(j,:) = [xup, 0];
+        j = j+1;
+        if j==3
+            plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','white');
+                continue;
         end
-        if (xdown ~= Inf) && (xdown ~= -Inf)
-            xy(j,:) = [xdown, size(I,2)];
-            j = j+1;
-                if j==3
-                    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','white');
-                    continue
-                end
+    end
+    if (xdown ~= Inf) && (xdown ~= -Inf)
+        xy(j,:) = [xdown + 1, s2 + 1];
+        j = j+1;
+        if j==3  
+            plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','white');
+            continue;
         end
-        if (yleft ~= Inf) && (yleft ~= -Inf)
-            xy(j,:) = [0, yleft];
-            j = j+1;
-                if j==3
-                    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','white');
-                    continue
-                end
+    end
+    if (yleft ~= Inf) && (yleft ~= -Inf)
+        xy(j,:) = [0, yleft + 1];
+        j = j+1;
+        if j==3
+            plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','white');
+            continue;
         end
-        if (yright ~= Inf) && (yright ~= -Inf)
-            xy(j,:) = [size(I,2), yright];
-            j = j+1;
-                if j==3
-                    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','white');
-                    continue
-                end
+    end
+    if (yright ~= Inf) && (yright ~= -Inf)
+        xy(j,:) = [s2 + 1, yright + 1];
+        j = j+1;
+        if j==3
+            plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','white');
+            continue;
         end
+    end
 end
+
 hold off;
 
 fig.Children.Units = 'pixels';
 image_pos = floor(fig.Children.Position);
 frame = getframe(gcf);
 frame_image_gray = rgb2gray(frame.cdata);
-cut_rows = (size(frame_image_gray, 1) - image_pos(2) - image_pos(4)):(size(frame_image_gray, 1) - image_pos(2) - 1);
-cut_cols = (size(frame_image_gray, 2) - image_pos(1) - image_pos(3)):(size(frame_image_gray, 2) - image_pos(1) - 1);
+% DEPENDING ON THE IMAGES IT MIGHT SHIFT A BIT AND FUCK UP THE FILLING
+cut_rows = (size(frame_image_gray, 1) - image_pos(2) - image_pos(4) + 2):(size(frame_image_gray, 1) - image_pos(2) + 1);
+cut_cols = (size(frame_image_gray, 2) - image_pos(1) - image_pos(3) + 2):(size(frame_image_gray, 2) - image_pos(1) + 1);
 I_burned = frame_image_gray(cut_rows, cut_cols);
 
 end
--- AdvancedTopicsOnComputerVision/ShortProject/pipe_detection_3.m
+++ AdvancedTopicsOnComputerVision/ShortProject/pipe_detection_3.m
@@ -6,55 +6,70 @@ number_of_pipes=1; % to create a function later, so far we will work with 1 only
 
 %% Pre-process image
 
-I = imread('Easy_pipes\pipe_2.jpg');
+I = imread('ezpipes\pipe3.jpg');
+% I = imread('easy_pipes\pipe_1.jpg');
+% IF YOU GET WARNING ABOUT IMAGE IS TO BIG THIS ALGO WONT WORK
+% IF YOU TRY NEW IMAGES AND THINGS CRASH CHECK THE FOLLOWING:
+% - CHECK THAT THE HOUGH LINE PLOTTER DRAWS THE LINE ALL THE WAY TO EDGE
+% - CHECK ALSO THAT THE CROP IN HOUGHLINES PLOTTER DOESN'T FUCK UP THE CROP
 g = imresize(rgb2gray(I), 0.5);
 
 %% Edge detection
 
-edges = edge(g,'Sobel', [], 'both', 'nothinning');
-%edges = edge(g, 'log');
-figure; imshowpair(g, edges, 'montage'); title('Image after Sobel edges');
+edges = edge(g,'Sobel', 'nothinning');
+%figure; imshowpair(g, edges, 'montage');
 
 %% Hough transform
 
-[H,theta,rho] = hough(edges,'RhoResolution',0.5,'Theta',-90:1:89);
+[H,theta,rho] = hough(edges,'RhoResolution',0.5,'Theta',-90:0.5:89.50);
 number_of_lines= number_of_pipes*2;
 peaks  = houghpeaks(H,number_of_lines,'threshold',ceil(0.3*max(H(:))));  % play with the number of peaks
-%peaks  = houghpeaks(H,number_of_lines);
 lines = houghlines(edges,theta,rho,peaks,'FillGap',200,'MinLength',200); % play with the FillGap and MinLenght parameters
-%lines = houghlines(edges,theta,rho,peaks);
-%% Plot Hough lines cuts with the axis
 
-bw = addHoughLines(g, lines);
-figure;imshow(bw);
+g_lines = addHoughLines(g, lines);
 
-%% Morphologic operations
+% POTENTIAL ADDITION IF TIME:
+% CONFIRM THAT ITS A PIPE AND NOT TWO RANDOMLY PARALLELL LINES
 
-se_opening = strel('square',3);
-opening = imopen(bw, se_opening); % opening to eliminate everything but the pipe
-figure; imshow(opening);
+%% Isolate cracks
 
-se_closing = strel('square',50); % fill the pipe with white pixels
-closing = imclose(opening, se_closing);
-figure; imshow(closing); title('Image after morphologic operations');
+pipe_edges = imbinarize(g_lines, 0.99); % The lines are drawn in complete white, so we can threshold almost everything
+% figure; imshowpair(g_lines, pipe_edges, 'montage');
 
-segmentation = closing.*bw; % apply the closing mask on the edges image + the hough lines
-figure; imshow(segmentation); title('Segmented pipe');
+% Now we want a mask to look at the pipes irregularities
+% PLAN
+% - Fill in between lines
+% - Use that to mask the original image
+% - Do another edge detection to get only edges on the pipe
 
-figure; imshow(segmentation - imopen(segmentation, ones(3)))
+% To fill the pipe we need to know where it is:
+% Assume a pipe defined by size(hough_lines) = 2, where hough_lines is the
+% struct returned by the houghlines()
+filled = fillPipe(pipe_edges, lines);
+% figure; imshow(filled);
+
+% We mask out the original image
+masked = g.*uint8(filled);
+cracks = imfilter(masked, fspecial('sobel')*fspecial('sobel')');
+cracks = imbinarize(cracks, 0.6); % Need to find a good threshold
+figure; imshowpair(masked, cracks, 'montage');
 
 %% Crack evaluation
-stats = regionprops(segmentation);
-% figure; imshow(g);
-% hold on;
-% for k = 1 : length(stats) % Loop through all blobs.
-% 	% Find the bounding box of each blob.
-% 	thisBlobsBoundingBox = stats(k).BoundingBox;  
-% 	rectangle('Position', thisBlobsBoundingBox);
-% end
+stats = regionprops(cracks);
+figure; imshow(g);
+hold on;
+for k = 1 : length(stats) % Loop through all blobs.
+	% Find the bounding box of each blob.
+	thisBlobsBoundingBox = stats(k).BoundingBox;  
+	rectangle('Position', thisBlobsBoundingBox, 'edgecolor', 'red');
+end
 
-% figure; imshow(BW);
-% mask = boundarymask(BW);
+% IDEA
+% - Since not all boxes are connected, make a box search (size % of radius)
+% - If (pixel area > thresh) -> crack detected!
+
+% figure; imshow(cracks);
+% mask = boundarymask(cracks);
 % imshow(mask)
 % regions = splitmerge(g, 4, @predicate)
 % imshow(regions);
